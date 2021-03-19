c39ed5ed21ed5dfde554b19342ba36c927ae3837
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-19 18:41:15 +0100

    myRecursion

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,20 +1,17 @@
+var arr = [1, 2, 3, 4, 5, 6];
 
 function myRecursion(arr, n) {
     
         // Only change code below this line
     
-    if (n <= 0) {
-        return 0;
-
-    } else {
-        return ((arr, n - 1) + arr[n - 1]);
+    if (n <= 0) { return 0;}
+    else { return (arr[n - 1] + myRecursion(arr, n - 1))};
         // Only change code above this line
-    }
     
-}
-console.log(myRecursion([1], 0)); 
-console.log(myRecursion([1, 2, 3, 4], 2));
-console.log(myRecursion([1, 2, 3, 4], 3));
-console.log(myRecursion([1, 2, 3, 4, 5, 6], 5));
+};
+console.log(myRecursion(arr,0)); 
+console.log(myRecursion(arr,2));
+console.log(myRecursion(arr,3));
+console.log(myRecursion(arr,5));
 
 module.exports = myRecursion;   
