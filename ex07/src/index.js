6ffe7962fe38d7a036c971a0dcf6e9e6d2ac6732
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-19 23:57:45 +0100

    changes on ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,47 +1,30 @@
-var contacts = [{
-    "firstName": "John",
-    "lastName": "Doe",
-    "number": "12365495208",
-    "likes": ["C", "C++", "C#"]
-},
-    { "firstName": "Jane",
-      "lastName": "Doe",
-      "number": "4568295761",
-      "likes": ["Phyton", "Swift", "R"]
-},
-{      "firstName": "Richard",
-        "lastName": "Roe",
-        "number": "6938257149",
-        "likes": ["React", "Angular", "Vue"] 
+var collection = {
+    2548: {
+        album: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: [
+            "Let it Rock", 
+            "You Give Love a Bad Name"
+        ]
     },
-{     "firstName": "Jane",
-      "lastName": "Roe",
-       "number": "unkown",
-        "likes": ["JavaScript", "Node", "HTML & CSS"]}
-];
-function lookUpProfile(name, prop) {
-    // Only change code below this line
-    var value;
-    for (var i = 0; i < contacts.length; i++){
-      if((contacts[i].firstName === name) && 
-         (contacts[i].hasOwnProperty(prop))){
-        value = contacts[i][prop];
-        break;
+    2468: {
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+            "1999", 
+            "Little Red Corvette"
+        ]
+    },
+    1245: {
+        artist: "Rober Palmer",
+        tracks: []
+    },
+};
+    // Keep a copy of the collection for test
+    var oldCollection = JSON.parse(JSON.stringify(collection));
+    function updateRecords(object, id, prop, value) {
+        // Only change code below this line
+        // Only change code above this line
     }
-    if (contacts[i].hasOwnProperty(name) === false){
-      value = "No such contact";
-    }
-    if (contacts[i].hasOwnProperty(prop) === false) {
-      value = "No such property";
-    }
-    }
-    return value;
-    }
-    // Only change code above this line
-console.log(lookUpProfile("John", "likes")); // Change this line
-console.log(lookUpProfile("Jane", "lastName"));
-console.log(lookUpProfile("Richard", "likes"));
-console.log(lookUpProfile("Rob", "number"));
-console.log(lookUpProfile("Rob", "JavaScript"));
-console.log(lookUpProfile("John", "adress"));
-module.exports = lookUpProfile;
+console.log(updateRecords(collection, id, prop, value)); // Change this line
+module.exports = updateRecords;
