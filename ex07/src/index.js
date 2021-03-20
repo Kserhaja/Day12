e06324ee03c6f5ddc746c5858379f3db44477100
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-20 10:16:37 +0100

    music collection

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,30 +1,43 @@
 var collection = {
     2548: {
-        album: "Slippery When Wet",
-        artist: "Bon Jovi",
-        tracks: [
-            "Let it Rock", 
-            "You Give Love a Bad Name"
-        ]
+      album: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: ["Let It Rock", "You Give Love a Bad Name"],
     },
     2468: {
-        album: "1999",
-        artist: "Prince",
-        tracks: [
-            "1999", 
-            "Little Red Corvette"
-        ]
+      album: "1999",
+      artist: "Prince",
+      tracks: ["1999", "Little Red Corvette"],
     },
     1245: {
-        artist: "Rober Palmer",
-        tracks: []
+      artist: "Robert Palmer",
+      tracks: [],
     },
-};
-    // Keep a copy of the collection for test
-    var oldCollection = JSON.parse(JSON.stringify(collection));
-    function updateRecords(object, id, prop, value) {
-        // Only change code below this line
-        // Only change code above this line
+    5439: {
+      album: "ABBA Gold",
+    },
+  };
+  // Keep a copy of the collection for tests
+  var collectionCopy = JSON.parse(JSON.stringify(collection));
+  // Only change code below this line
+  function updateRecords(object, id, prop, value) {
+    if (value === "") {
+      delete collection[id][prop];
+    } else if (prop !== "tracks") {
+      collection[id][prop] = value;
+    } else {
+      if (!collection[id].hasOwnProperty("tracks")) {
+        collection[id].tracks = [];
+        collection[id].tracks.push(value);
+      } else {
+        collection[id].tracks.push(value);
+      }
     }
-console.log(updateRecords(collection, id, prop, value)); // Change this line
-module.exports = updateRecords;
+    return collection;
+    // Only change code above this line
+  }
+  console.log(updateRecords(collection, 5439, "artist", "ABBA"));         // Change this line
+  console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
+  console.log(updateRecords(collection, 2548, "artist", ""));
+  console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
+  module.exports = updateRecords;
